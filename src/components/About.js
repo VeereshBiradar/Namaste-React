6475b17ba12138cd3cabacec95904dcc9fbad631
Veeresh Biradar <veereshbiradar1998@gmail.com>
2024-03-25 18:39:23 +0530

    Class-based-components-first-commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,38 @@
-const About = () => {
-  return (
-    <>
-      <h1>Know about me then click on the page </h1>
-    </>
-  );
-};
-export default About;
+import { Component } from "react";
+import { User } from "./User";
+import UserClass from "./UserClass";
+
+export class About extends Component {
+  constructor(props){
+    super(props)
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount(){
+    console.log("Parent  componentDidMount()")
+  }
+
+  render(){
+
+    console.log('Parent Render')
+    return (
+      <div>
+         <h1>Know about me then click on the page </h1>
+         <UserClass name={'Veeresh Biradar (ClassBased)'} location={'Bidar'}/>
+      </div>
+    )
+  }
+}
+
+// const About = () => {
+//   return (
+//     <>
+//       <h1>Know about me then click on the page </h1>
+//       {/* <User name={'Veeresh Biradar (Functional)'}/> */}
+//       <br/>
+//       <UserClass name={'Veeresh Biradar (ClassBased)'} location={'Bidar'}/>
+//     </>
+//   );
+// };
+// export default About;
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+export const User = (props) => {
+    return (
+        <div className="user-card">
+            <h1>Name: {props.name}</h1>
+            <h2>Locaion: Bidar</h2>
+            <h3>Profession: Software Engineer</h3>
+        </div>
+    )
+}
